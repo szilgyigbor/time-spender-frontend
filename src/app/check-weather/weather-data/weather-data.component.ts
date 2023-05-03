bd329a4ffa61d3fb41d150a3c237694e1e2cffea
Gábor <szilgyigbor@gmail.com>
2023-05-03 21:58:58 +0200

    Weathe-data component is added.

---
+++ src/app/check-weather/weather-data/weather-data.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'tisp-weather-data',
+  templateUrl: './weather-data.component.html',
+  styleUrls: ['./weather-data.component.css']
+})
+export class WeatherDataComponent {
+
+}
