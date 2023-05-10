c186fdbf317e100f1375568a6d0feb3f462b9487
Gábor <szilgyigbor@gmail.com>
2023-05-10 10:20:59 +0200

    Unused HttpClient injection is removed from constructors.

--- src/app/check-weather/check-weather.component.ts
+++ src/app/check-weather/check-weather.component.ts
@@ -1,7 +1,8 @@
 import { Component } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { LocationItem, WeatherItem } from '../interfaces/weather-item';
-import { FlickrImage } from '../interfaces/flickr-image';
+
+import { PostRequestsService } from '../services/post-requests.service';
+import { WeatherItem } from '../interfaces/weather-item';
+
 
 @Component({
   selector: 'tisp-check-weather',
@@ -15,51 +16,29 @@ export class CheckWeatherComponent {
   imageUrl: string = '';
   weatherItems: WeatherItem = {} as WeatherItem;
 
-  constructor(private http: HttpClient) { 
+  constructor(private postRequestsService: PostRequestsService) { 
   }
 
   sendLocation() {
     console.log('Location:', this.location);
-    this.getImageUrl(this.location);
-    this.getWeatherData(this.location);
+    this.postRequestsService.postImageRequest(this.location).subscribe((data: any) => {
+      this.pictureDatas = data;
+      const imageUrl = this.pictureDatas.sizes.size.find((size: { label: string; }) => size.label === "Large").source;
+      this.imageUrl = `url('${imageUrl}')`;
+    });
+    
+    this.postRequestsService.postWeatherRequest(this.location).subscribe((data: any) => {
+      console.log('WeatherData:', data);
+      this.weatherItems = data as WeatherItem;
+    });
     this.location = '';
   }
 
   
-  getImageUrl(location: string) {
-    this.http.post('/api/getimage', JSON.stringify(location), {
-      headers: {
-        'Content-Type': 'application/json'
-      }
-    })
-      .subscribe((data) => {
-        this.pictureDatas = data
-        const imageUrl = this.pictureDatas.sizes.size.find((size: { label: string; }) => size.label === "Large").source;
-        this.imageUrl = `url('${imageUrl}')`;
-        console.log('Image url:', this.imageUrl);
-      });
-  }
-
-  getWeatherData(location: string) {
-    this.http.post('/api/getweather', JSON.stringify(location), {
-      headers: {
-        'Content-Type': 'application/json'
-      }
-    })
-      .subscribe((data: any) => {
-      console.log('WeatherData:', data);
-      this.weatherItems = data as WeatherItem;
-      console.log('WeatherData:', this.weatherItems.location.country);
-      console.log('WeatherData:', this.weatherItems.location.name);
-      console.log('WeatherData:', this.weatherItems.current.temp_c);
-      console.log('WeatherData:', this.weatherItems.current.condition.icon);
-      console.log('WeatherData:', this.weatherItems.current.condition.text);
-    });
-  }
+  
 
 
   ngOnInit(): void {
-    //this.sendLocation();
   }
 
 }
