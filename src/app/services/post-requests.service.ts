da56813aa53b50c63f2d3e0c694b90c1cb6b89ea
Gábor <szilgyigbor@gmail.com>
2023-05-17 21:15:57 +0200

    SendMessageHistory is added to post-service.

--- src/app/services/post-requests.service.ts
+++ src/app/services/post-requests.service.ts
@@ -28,7 +28,15 @@ export class PostRequestsService {
     const formData = new FormData();
     formData.append('audioFile', file);
   
-    return this.http.post('http://localhost:5000/transcribe', formData);
-}
+    return this.http.post('http://localhost:5000/api2/transcribe', formData);
+  }
+
+  sendMessageHistory(history: {role: string, content: string}[]) {
+    return this.http.post<{role: string, content: string}[]>('http://localhost:5000/api2/get-answer', JSON.stringify(history), {
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    });
+  }
 
 }
