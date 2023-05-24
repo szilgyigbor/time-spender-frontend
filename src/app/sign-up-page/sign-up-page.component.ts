e57c7a3a394e116e6bcbefe418295ad21cdf1fa8
Gábor <szilgyigbor@gmail.com>
2023-05-24 10:01:58 +0200

    SendSignUp function of sign up page component now sends the datas to the backend.

--- src/app/sign-up-page/sign-up-page.component.ts
+++ src/app/sign-up-page/sign-up-page.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { PostRequestsService } from '../services/post-requests.service';
+import { SignUpData } from '../interfaces/sign-up-data';
 
 @Component({
   selector: 'tisp-sign-up-page',
@@ -7,31 +9,33 @@ import { Component } from '@angular/core';
 })
 export class SignUpPageComponent {
 
-  username: string = '';
-  password: string = '';
-  email: string = '';
+  signUpData: SignUpData = {} as SignUpData;
   passwordConfirm: string = '';
 
-  constructor() { 
+  constructor(private postRequestsService: PostRequestsService) { 
   }
 
   ngOnInit(): void {}
 
   sendSignUp() {
 
-    if (this.password !== this.passwordConfirm) {
+    if (this.signUpData.password !== this.passwordConfirm) {
       alert('Passwords do not match!');
       return;
     }
 
-    console.log('Username:', this.username);
-    console.log('Password:', this.password);
-    console.log('Email:', this.email);
-    
+    console.log('Username:', this.signUpData.username);
+    console.log('Password:', this.signUpData.password);
+    console.log('Email:', this.signUpData.email);
 
-    this.username = '';
-    this.password = '';
-    this.email = '';
+    this.postRequestsService.sendSignUpData(this.signUpData).subscribe(response => {
+      console.log('SignUpData:', response);
+    });
+
+
+    this.signUpData.username = '';
+    this.signUpData.password = '';
+    this.signUpData.email = '';
     this.passwordConfirm = '';
   }
 
