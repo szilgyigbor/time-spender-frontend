452f9e5067503d7cc15d833a9b0fed202fac4acd
Gábor <szilgyigbor@gmail.com>
2023-06-12 19:36:22 +0200

    Jwt.interceptor.ts file is added.

---
+++ src/app/interceptors/jwt.interceptor.ts
@@ -0,0 +1,17 @@
+import { HttpInterceptor, HttpRequest, HttpHandler } from '@angular/common/http';
+
+export class JwtInterceptor implements HttpInterceptor {
+    intercept(request: HttpRequest<any>, next: HttpHandler) {
+        // get token from local storage
+        let currentUser = JSON.parse(localStorage.getItem('currentUser')!);
+        if (currentUser && currentUser.access_token) {
+            request = request.clone({
+                setHeaders: {
+                    Authorization: `Bearer ${currentUser.access_token}`
+                }
+            });
+        }
+
+        return next.handle(request);
+    }
+}
