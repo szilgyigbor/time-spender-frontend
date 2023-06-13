16b650ffd5cc01953ac0e661a2e50e6fa10d9db7
Gábor <szilgyigbor@gmail.com>
2023-06-13 11:57:52 +0200

    Login page is refactored, many unnecessary lines are deleted.

--- src/app/login-page/login-page.component.ts
+++ src/app/login-page/login-page.component.ts
@@ -12,41 +12,22 @@ import { Router } from '@angular/router';
 export class LoginPageComponent {
 
   loginData: UserData = {} as UserData;
-  username: string = '';
-  password: string = '';
 
   constructor(private postRequestsService: PostRequestsService, private loginService: LoginService, 
     private router: Router) { 
   }
 
   sendLogin() {
-
-    this.loginData.username = this.username;
-    this.loginData.password = this.password;
-    console.log('Username:', this.username);
-    console.log('Password:', this.password);
-
+   
     this.postRequestsService.sendLoginData(this.loginData).subscribe(response => {
       console.log('LoginData:', response);
       localStorage.setItem('currentUser', JSON.stringify(response));
-
-      let currentUser = JSON.parse(localStorage.getItem('currentUser')!);
-      console.log(currentUser.username);
-      console.log(currentUser.email);
       this.loginService.setLoggedIn(true);
       this.router.navigate(['/']);
-
     });
-
-
-    this.username = '';
-    this.password = '';
-
-    
   }
 
   ngOnInit(): void {
-    
   }
 
 }
