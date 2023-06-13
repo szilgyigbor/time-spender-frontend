5033a7512a488dbaa7c876d02d5b27b993efb29d
Gábor <szilgyigbor@gmail.com>
2023-06-13 11:26:32 +0200

    Login service is added.

---
+++ src/app/services/login.service.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
+
+@Injectable({ providedIn: 'root' })
+export class LoginService {
+    private _isLoggedIn = new BehaviorSubject<boolean>(this.isSomeone());
+    isLoggedIn$ = this._isLoggedIn.asObservable();
+
+    private isSomeone(): boolean {
+        return !!localStorage.getItem('currentUser');
+      }
+
+    setLoggedIn(val: boolean) {
+        this._isLoggedIn.next(val);
+    }
+}
