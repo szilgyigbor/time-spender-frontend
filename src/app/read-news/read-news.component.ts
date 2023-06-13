16b0bcef8a741bc4b24bf5c509b453b5f7fee617
Gábor <szilgyigbor@gmail.com>
2023-06-13 12:12:15 +0200

    Error handling at the response of the request: If you are not logged in, you will be redirected to the login page.

--- src/app/read-news/read-news.component.ts
+++ src/app/read-news/read-news.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { NewsItem } from '../interfaces/news-item';
 import { GetRequestsService } from '../services/get-requests.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'tisp-read-news',
@@ -11,14 +12,22 @@ export class ReadNewsComponent {
   title: string = 'time-spender-frontend';
   news: NewsItem[] = [];
 
-  constructor(private getRequestsService: GetRequestsService) { }
+  constructor(private getRequestsService: GetRequestsService, private router: Router ) { }
 
   ngOnInit(): void {
     
     this.getRequestsService.getNewsRequest().subscribe((data: any) => {
       console.log('News data:', data.articles);
       this.news = data.articles;
-    });
+    },
+    error => {
+      if (error.status === 401) {
+        alert("Please, login!");
+        this.router.navigate(['/login']);
+      }
+    }
+    
+    );
   }
  
 }
