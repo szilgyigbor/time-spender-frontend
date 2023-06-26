f5507cee7d5af891e73073e92598676400b85b7d
Gábor <szilgyigbor@gmail.com>
2023-06-26 11:07:30 +0200

    test(AppComponent): remove irrelevant generated tests

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,17 +20,5 @@ describe('AppComponent', () => {
     const app = fixture.componentInstance;
     expect(app).toBeTruthy();
   });
-
-  it(`should have as title 'time-spender-frontend'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('time-spender-frontend');
-  });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('time-spender-frontend app is running!');
-  });
+  
 });
