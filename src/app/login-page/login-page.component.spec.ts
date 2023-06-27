1294cb0cb91190d7144a77420353637b63651b75
Gábor <szilgyigbor@gmail.com>
2023-06-27 09:46:03 +0200

    test(LoginComponent): Add test for sendLogin method

--- src/app/login-page/login-page.component.spec.ts
+++ src/app/login-page/login-page.component.spec.ts
@@ -2,6 +2,8 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { FormsModule } from '@angular/forms';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { LoginPageComponent } from './login-page.component';
+import { PostRequestsService } from '../services/post-requests.service';
+import { of } from 'rxjs';
 
 describe('LoginPageComponent', () => {
   let component: LoginPageComponent;
@@ -10,7 +12,8 @@ describe('LoginPageComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [ LoginPageComponent ],
-      imports: [ HttpClientTestingModule, FormsModule ],
+      imports: [ HttpClientTestingModule, FormsModule],
+      providers: [ PostRequestsService ]
     })
     .compileComponents();
 
@@ -19,7 +22,18 @@ describe('LoginPageComponent', () => {
     fixture.detectChanges();
   });
 
+
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+
+  it('should call sendLoginData with the correct argument', () => {
+    const fakeResponse = {access_token: 'test', expires_at: 'test', username: 'test', email: 'test@gmail.com'};
+    const postRequestsService = TestBed.inject(PostRequestsService);
+    spyOn(postRequestsService, 'sendLoginData').and.returnValue(of(fakeResponse));
+    component.sendLogin();
+    expect(postRequestsService.sendLoginData).toHaveBeenCalledWith(component.loginData);
+  });
+
 });
