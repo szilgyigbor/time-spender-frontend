85a88e04c54c1fcc45f6dcf2d38e6edb1024d86a
Gábor <szilgyigbor@gmail.com>
2023-06-29 18:13:04 +0200

    test(signUpComponent): Add test for navigation to homepage

--- src/app/sign-up-page/sign-up-page.component.spec.ts
+++ src/app/sign-up-page/sign-up-page.component.spec.ts
@@ -4,6 +4,9 @@ import { PostRequestsService } from '../services/post-requests.service';
 import { FormsModule } from '@angular/forms';
 
 import { SignUpPageComponent } from './sign-up-page.component';
+import { Router } from '@angular/router';
+import { of } from 'rxjs';
+import { LoginService } from '../services/login.service';
 
 describe('SignUpPageComponent', () => {
   let component: SignUpPageComponent;
@@ -13,7 +16,7 @@ describe('SignUpPageComponent', () => {
     await TestBed.configureTestingModule({
       declarations: [ SignUpPageComponent ],
       imports: [ HttpClientTestingModule, FormsModule ],
-      providers: [ PostRequestsService ],
+      providers: [ PostRequestsService, Router, LoginService ],
     })
     .compileComponents();
 
@@ -22,7 +25,24 @@ describe('SignUpPageComponent', () => {
     fixture.detectChanges();
   });
 
+
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+
+  it('should navigate to the homepage', () => {
+    const fakeResponse = {access_token: 'test', expires_at: 'test', username: 'test', email: 'test@gmail.com'};
+    const postRequestsService = TestBed.inject(PostRequestsService);
+    spyOn(postRequestsService, 'sendSignUpData').and.returnValue(of(fakeResponse));
+    const router = TestBed.inject(Router);
+    spyOn(router, 'navigate');
+    component.signUpData = { username: 'test', email: 'test@email.com', password: 'test'};
+    component.passwordConfirm = 'test';
+    component.sendSignUp();
+  
+    expect(router.navigate).toHaveBeenCalledWith(['/']);
+  });
+ 
+
 });
