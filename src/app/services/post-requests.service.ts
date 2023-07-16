433d0540f3bf4870e1aa73108fa7b45c73e15847
Gábor <szilgyigbor@gmail.com>
2023-07-16 11:47:47 +0200

    Change the urls of post and get requests .

--- src/app/services/post-requests.service.ts
+++ src/app/services/post-requests.service.ts
@@ -10,15 +10,26 @@ export class PostRequestsService {
   constructor(private http: HttpClient) { }
 
   postImageRequest(location: string) {
-    return this.http.post('https://spender-backend-7753b2e4b87a.herokuapp.com/api/getimage', JSON.stringify(location), {
+    /*return this.http.post('https://spender-backend-7753b2e4b87a.herokuapp.com/api/getimage', JSON.stringify(location), {
+      headers: {
+        'Content-Type': 'application/json'
+      }*/
+
+    return this.http.post('http://localhost:23982/api/getimage', JSON.stringify(location), {
       headers: {
         'Content-Type': 'application/json'
       }
+
     });
   }
 
   postWeatherRequest(location: string) {
-    return this.http.post('https://spender-backend-7753b2e4b87a.herokuapp.com/api/getweather', JSON.stringify(location), {
+    /*return this.http.post('https://spender-backend-7753b2e4b87a.herokuapp.com/api/getweather', JSON.stringify(location), {
+      headers: {
+        'Content-Type': 'application/json'
+      }*/
+
+    return this.http.post('http://localhost:23982/api/getweather', JSON.stringify(location), {
       headers: {
         'Content-Type': 'application/json'
       }
@@ -29,11 +40,18 @@ export class PostRequestsService {
     const formData = new FormData();
     formData.append('audioFile', file);
   
-    return this.http.post('https://spender-gptbackend-66fcf581c179.herokuapp.com/pia/transcribe', formData);
+    /*return this.http.post('https://spender-gptbackend-66fcf581c179.herokuapp.com/pia/transcribe', formData);*/
+
+    return this.http.post('http://localhost:5000/pia/transcribe', formData);
   }
 
   sendMessageHistory(history: {role: string, content: string}[]) {
-    return this.http.post<{role: string, content: string}[]>('https://spender-gptbackend-66fcf581c179.herokuapp.com/pia/get-answer', JSON.stringify(history), {
+    /*return this.http.post<{role: string, content: string}[]>('https://spender-gptbackend-66fcf581c179.herokuapp.com/pia/get-answer', JSON.stringify(history), {
+      headers: {
+        'Content-Type': 'application/json'
+      }*/
+
+    return this.http.post<{role: string, content: string}[]>('http://localhost:5000/pia/get-answer', JSON.stringify(history), {
       headers: {
         'Content-Type': 'application/json'
       }
@@ -41,7 +59,12 @@ export class PostRequestsService {
   }
 
   sendSignUpData(signUpData: UserData) { 
-    return this.http.post('https://spender-backend-7753b2e4b87a.herokuapp.com/api/user/signup', JSON.stringify(signUpData), {
+    /*return this.http.post('https://spender-backend-7753b2e4b87a.herokuapp.com/api/user/signup', JSON.stringify(signUpData), {
+      headers: {
+        'Content-Type': 'application/json'
+      }*/
+
+    return this.http.post('/api/user/signup', JSON.stringify(signUpData), {
       headers: {
         'Content-Type': 'application/json'
       }
@@ -49,7 +72,12 @@ export class PostRequestsService {
    }
 
    sendLoginData(loginData: UserData) { 
-    return this.http.post('https://spender-backend-7753b2e4b87a.herokuapp.com/api/user/login', JSON.stringify(loginData), {
+    /*return this.http.post('https://spender-backend-7753b2e4b87a.herokuapp.com/api/user/login', JSON.stringify(loginData), {
+      headers: {
+        'Content-Type': 'application/json'
+      }*/
+
+    return this.http.post('/api/user/login', JSON.stringify(loginData), {
       headers: {
         'Content-Type': 'application/json'
       }
