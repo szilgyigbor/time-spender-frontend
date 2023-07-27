3b81ea4eca29e35d8328e98afc5c3a542a2f7b25
Gábor <szilgyigbor@gmail.com>
2023-07-27 11:45:00 +0200

    Upgrade(signalr.service.ts): Add isConnectionStarted variable Add isUpdating variable Add startUpdateingStatus function Add stopUpdateingStatus function Add updateingStatus function

--- src/app/services/signalr.service.ts
+++ src/app/services/signalr.service.ts
@@ -1,7 +1,7 @@
 import { Injectable, OnInit } from '@angular/core';
 import * as signalR from '@aspnet/signalr';
 import { HubConnection, HubConnectionBuilder } from '@aspnet/signalr';
-import { Subject } from 'rxjs';
+import { BehaviorSubject, Subject } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -13,7 +13,13 @@ export class SignalrService {
 
   private characterMovedSource = new Subject<any>();
   characterMoved$ = this.characterMovedSource.asObservable();
-  
+
+  private isConnectionStarted = new BehaviorSubject<boolean>(false);
+  isConnectionStarted$ = this.isConnectionStarted.asObservable();
+
+  private isUpdating: boolean;
+
+  //BASE_URL = 'https://spender-backend-7753b2e4b87a.herokuapp.com';
   BASE_URL = 'https://localhost:44320';
 
   constructor() {
@@ -21,6 +27,7 @@ export class SignalrService {
     .withUrl(this.BASE_URL + '/gamehub')
     .configureLogging(signalR.LogLevel.Information)  // Enable logging
     .build();
+    this.isUpdating = false;
   }
 
 
@@ -28,14 +35,21 @@ export class SignalrService {
 
     this.hubConnection
       .start()
-      .then(() => console.log('Connection started'))
+      .then(() => {
+        console.log('Connection started');
+        this.isConnectionStarted.next(true);
+    })
       .catch(err => console.log('Error while starting connection: ' + err))
   }
 
   public stopConnection = () => {
     this.hubConnection
       .stop()
-      .then(() => console.log('Connection stopped'))
+      .then(() => {
+        console.log('Connection stopped')
+        this.isConnectionStarted.next(false);
+        this.characterMovedSource.next([]);
+      })
       .catch(err => console.log('Error while stopping connection: ' + err))
   }
 
@@ -52,7 +66,29 @@ export class SignalrService {
 
   public movePlayer = (username: string, positionX: number, positionY: number) => {
     this.hubConnection
-        .invoke('MoveCharacter', username, positionX, positionY)
-        .catch(err => console.error(err));
+      .invoke('MoveCharacter', username, positionX, positionY)
+      .catch(err => console.error(err));
+  }
+
+  public async startUpdatingStatus() {
+    this.isUpdating = true;
+    while (this.isUpdating) {
+        try {
+            await this.updateStatus();
+        } catch (error) {
+            console.error(error);
+        }
+        await new Promise(resolve => setTimeout(resolve, 100));
     }
+  }
+
+  public stopUpdatingStatus() {
+    this.isUpdating = false;
+  }
+
+  public updateStatus = () => {
+    this.hubConnection
+      .invoke('UpdateStatus')
+      .catch(err => console.error(err));
+  } 
 }
