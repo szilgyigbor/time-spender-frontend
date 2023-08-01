a7fd0ac86904de4dfc44fdf16989c68ced820bd5
Gábor <szilgyigbor@gmail.com>
2023-08-01 14:08:12 +0200

    Add backend_for_signal_url variable to environment files

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,6 @@
 export const environment = {
     production: true,
     backend_url: 'https://spender-backend-7753b2e4b87a.herokuapp.com',
+    backend_for_signal_url: 'https://spender-backend-7753b2e4b87a.herokuapp.com',
     gpt_backend_url: 'https://spender-gptbackend-66fcf581c179.herokuapp.com'
   };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,6 @@
 export const environment = {
     production: false,
     backend_url: '',
+    backend_for_signal_url: 'https://localhost:44320',
     gpt_backend_url: 'http://localhost:5000'
   };
