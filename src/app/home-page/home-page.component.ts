d9dab1594bf861ae736c0810c3a586215e3c36fc
Gábor <szilgyigbor@gmail.com>
2023-08-08 22:45:57 +0200

    Upgrade(Home page): - Add a random news to the homepage with design.

--- src/app/home-page/home-page.component.ts
+++ src/app/home-page/home-page.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { GetRequestsService } from '../services/get-requests.service';
+import { NewsItem } from '../interfaces/news-item';
 
 @Component({
   selector: 'tisp-home-page',
@@ -7,5 +9,21 @@ import { Component } from '@angular/core';
 })
 export class HomePageComponent {
   title = 'time-spender-frontend';
+  randomNews: NewsItem = {} as NewsItem;
+
+  constructor(private getRequestsService: GetRequestsService) {
+  }
+
+  ngOnInit(): void {
+    this.getANews();
+  }
+
+  getANews() {
+    this.getRequestsService.getOneNewsRequest().subscribe((data: any) => {
+      console.log(data);
+      this.randomNews = data;
+    });
+  }
+
 
 }
