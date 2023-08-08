26e45907d938793374697b92fbe70bfdbe45b3bb
Gábor <szilgyigbor@gmail.com>
2023-08-08 20:26:03 +0200

    Upgrade(read-news.component): - Add a new alert message to error 401 status.

--- src/app/read-news/read-news.component.ts
+++ src/app/read-news/read-news.component.ts
@@ -27,9 +27,16 @@ export class ReadNewsComponent {
       },
       error: error => {
         if (error.status === 401) {
-          localStorage.removeItem('currentUser');
-          alert("Please, login!");
-          this.router.navigate(['/login']);
+          if (!!localStorage.getItem('currentUser') ==  true)
+          {
+            localStorage.removeItem('currentUser');
+            alert("Your token has expired, please login!");
+            this.router.navigate(['/login']);
+          }
+          else {
+            alert("Please, login!");
+            this.router.navigate(['/login']);
+          }
         }
       }
     });
