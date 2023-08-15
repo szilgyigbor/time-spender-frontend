29438e59822ccb1718c4d7b6febeaf4d75307156
Gábor <szilgyigbor@gmail.com>
2023-08-15 10:55:30 +0200

    Add onDrop event.

--- src/app/number-sorting/number-sorting.component.ts
+++ src/app/number-sorting/number-sorting.component.ts
@@ -21,4 +21,12 @@ export class NumberSortingComponent {
     }
     return numbers;
   }
+
+  onDrop(event: CdkDragDrop<number[]>) {
+    if (event.previousContainer === event.container) {
+      moveItemInArray(event.container.data, event.previousIndex, event.currentIndex);
+    } else {
+      transferArrayItem(event.previousContainer.data, event.container.data, event.previousIndex, event.currentIndex);
+    }
+  }
 }
