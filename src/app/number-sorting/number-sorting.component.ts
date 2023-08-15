124c8c246ebdbcefcf101bda4701385e155ad0b8
Gábor <szilgyigbor@gmail.com>
2023-08-15 10:53:11 +0200

    Upgrade(number-sorintg.component.ts): - Import modules from angular drag and drop.

--- src/app/number-sorting/number-sorting.component.ts
+++ src/app/number-sorting/number-sorting.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { CdkDragDrop, moveItemInArray, transferArrayItem } from '@angular/cdk/drag-drop';
 
 @Component({
   selector: 'tisp-number-sorting',
