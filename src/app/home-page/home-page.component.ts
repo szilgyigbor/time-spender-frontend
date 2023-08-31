6e41b2dbea2606f445ab3f08bc7bfd78d3835fa6
Gábor <szilgyigbor@gmail.com>
2023-08-31 21:57:35 +0200

    Remove unnecessary getANews function.

--- src/app/home-page/home-page.component.ts
+++ src/app/home-page/home-page.component.ts
@@ -1,6 +1,4 @@
 import { Component } from '@angular/core';
-import { GetRequestsService } from '../services/get-requests.service';
-import { NewsItem } from '../interfaces/news-item';
 
 @Component({
   selector: 'tisp-home-page',
@@ -9,25 +7,15 @@ import { NewsItem } from '../interfaces/news-item';
 })
 export class HomePageComponent {
   title = 'time-spender-frontend';
-  randomNews: NewsItem = {} as NewsItem;
   backgroundImage = 'url("assets/background_app.jpg")';
   backgroundPosition = '0 0';
 
-  constructor(private getRequestsService: GetRequestsService) {
+  constructor() {
   }
 
   ngOnInit(): void {
-    this.getANews();
     const pageHeight = document.body.scrollHeight;
     this.backgroundPosition = `0 0, 0 ${pageHeight}px`;
-
   }
-
-  getANews() {
-    this.getRequestsService.getOneNewsRequest().subscribe((data: any) => {
-      this.randomNews = data;
-    });
-  }
-
-
+  
 }
