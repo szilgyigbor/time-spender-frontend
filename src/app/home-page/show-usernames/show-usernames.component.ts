0df470fe34e93d72fe4a7e23182244c03822c42d
Gábor <szilgyigbor@gmail.com>
2023-09-01 19:55:21 +0200

    Add getUsernamesRequests function to ngOnInit.

--- src/app/home-page/show-usernames/show-usernames.component.ts
+++ src/app/home-page/show-usernames/show-usernames.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { GetRequestsService } from 'src/app/services/get-requests.service';
 
 @Component({
   selector: 'tisp-show-usernames',
   templateUrl: './show-usernames.component.html',
   styleUrls: ['./show-usernames.component.css']
 })
-export class ShowUsernamesComponent {
+export class ShowUsernamesComponent implements OnInit {
+
+  usernames: string[] = [];
+
+  constructor(private getRequestService: GetRequestsService) { }
+
+  ngOnInit(): void {
+    this.getRequestService.getUsernamesRequest().subscribe((usernames: any) => {
+      this.usernames = usernames;
+    });
+  }
 
 }
+
