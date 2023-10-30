22a41dc096877e3d0d5af413dbd2296e41985c9e
Gábor <szilgyigbor@gmail.com>
2023-10-30 11:34:43 +0100

    Add single-post component

---
+++ src/app/forum/single-post/single-post.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'tisp-single-post',
+  templateUrl: './single-post.component.html',
+  styleUrls: ['./single-post.component.css']
+})
+export class SinglePostComponent {
+
+}
