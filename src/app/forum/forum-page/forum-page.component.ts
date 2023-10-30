87efa069581f738d56759f22f9297ff58a5868c5
Gábor <szilgyigbor@gmail.com>
2023-10-30 19:01:10 +0100

    Add goToPost function to forum-page component.

--- src/app/forum/forum-page/forum-page.component.ts
+++ src/app/forum/forum-page/forum-page.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 import { PostData } from 'src/app/interfaces/post-data';
 import { ForumService } from 'src/app/services/forum.service';
 
@@ -11,21 +12,23 @@ export class ForumPageComponent {
 
   allPosts: PostData[] = [];
 
-  constructor(private forumService: ForumService) { }
+  constructor(private forumService: ForumService, private router: Router) { }
 
   ngOnInit(): void {
 
     this.getPosts();
   }
 
-
   getPosts() {
     this.forumService.getPosts().subscribe(
       (data) => {
         this.allPosts = data;
-        console.log(data);
       }
     );
+  }
+
+  goToPost(id: number) {
+    this.router.navigate(['/forum', id]);
   }
 
 }
