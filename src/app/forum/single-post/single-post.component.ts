e28c07be68d96b3fa6255c47980f106f0607d5b2
Gábor <szilgyigbor@gmail.com>
2023-10-30 19:03:21 +0100

    Upgrade single-post.component: - Add getPostById function. - Use getPostById in ngOnInit. - Upgrade the design of it.

--- src/app/forum/single-post/single-post.component.ts
+++ src/app/forum/single-post/single-post.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { ForumService } from 'src/app/services/forum.service';
 
 @Component({
   selector: 'tisp-single-post',
@@ -6,5 +8,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./single-post.component.css']
 })
 export class SinglePostComponent {
+
+  constructor(private forumService: ForumService, private route: ActivatedRoute) {}
+
+  ngOnInit() {
+    const id = this.route.snapshot.params['id'];
+    console.log(id);
+    this.getPostById(id);
+  }
+
+  getPostById(id: number) {
+    this.forumService.getPostById(id).subscribe(
+      (data) => {
+        console.log(data);
+      }
+    );
+  }
 
 }
