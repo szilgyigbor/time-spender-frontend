8f605b9a4aec20984ab50fcf86d6731cef58f358
Gábor <szilgyigbor@gmail.com>
2023-10-31 06:48:13 +0100

    Add addComment function to single-post.

--- src/app/forum/single-post/single-post.component.ts
+++ src/app/forum/single-post/single-post.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { ForumService } from 'src/app/services/forum.service';
+import { PostData } from 'src/app/interfaces/post-data';
+import { CommentData } from 'src/app/interfaces/comment-data';
 
 @Component({
   selector: 'tisp-single-post',
@@ -9,10 +11,15 @@ import { ForumService } from 'src/app/services/forum.service';
 })
 export class SinglePostComponent {
 
+  currentPost: PostData = {} as PostData;
+  newComment: CommentData = {} as CommentData;
+  username: string = "";
+
   constructor(private forumService: ForumService, private route: ActivatedRoute) {}
 
   ngOnInit() {
     const id = this.route.snapshot.params['id'];
+    this.username = JSON.parse(localStorage.getItem('currentUser')!).username;
     console.log(id);
     this.getPostById(id);
   }
@@ -21,8 +28,24 @@ export class SinglePostComponent {
     this.forumService.getPostById(id).subscribe(
       (data) => {
         console.log(data);
+        this.currentPost = data;
       }
     );
+  }
+
+  addComment() {
+    this.newComment.postId = this.currentPost.id;
+    this.newComment.createdAt = new Date();
+    this.newComment.author = this.username;
+    this.forumService.addComment(this.newComment).subscribe(
+      (data) => {
+        console.log("Comment added");
+        console.log(data);
+        this.getPostById(this.currentPost.id);
+        this.newComment = {} as CommentData;
+      }
+    );
+    
   }
 
 }
