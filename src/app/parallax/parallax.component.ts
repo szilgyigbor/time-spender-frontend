4816f18302b75d41abfb2065ae6ab57c0072c42f
Gábor <szilgyigbor@gmail.com>
2023-11-19 23:23:29 +0100

    Add parallax effect to the component

--- src/app/parallax/parallax.component.ts
+++ src/app/parallax/parallax.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'tisp-parallax',
@@ -6,5 +6,34 @@ import { Component } from '@angular/core';
   styleUrls: ['./parallax.component.css']
 })
 export class ParallaxComponent {
+  
+  @ViewChild('text')
+  text!: ElementRef;
+
+  @ViewChild('tree_left')
+  treeLeft!: ElementRef;
+
+  @ViewChild('tree_right')
+  treeRight!: ElementRef;
+
+  @ViewChild('gate_left')
+  gateLeft!: ElementRef;
+
+  @ViewChild('gate_right')
+  gateRight!: ElementRef;
+
+  constructor() { }
+
+  ngOnInit(): void {
+    window.addEventListener('scroll', () => {
+      const value = window.scrollY;
+      this.text.nativeElement.style.marginTop = value * 2.5 + 'px';
+      this.treeLeft.nativeElement.style.left = value * -1.0 + 'px';
+      this.treeRight.nativeElement.style.left = value * 1.0 + 'px';
+      this.gateLeft.nativeElement.style.left = value * 0.3 + 'px';
+      this.gateRight.nativeElement.style.left = value * -0.3 + 'px';
+
+    });
+  }
 
 }
