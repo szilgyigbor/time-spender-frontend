f559168c49f0673a7952f133b45a791435dc0217
Gábor <szilgyigbor@gmail.com>
2023-11-22 22:09:40 +0100

    Add description animation to AboutMeComponent

--- src/app/about-me/about-me.component.ts
+++ src/app/about-me/about-me.component.ts
@@ -15,6 +15,28 @@ export class AboutMeComponent {
   }
 
   ngOnInit(): void {
+
+  async animateDescription() {
+    while (true) {
+
+      this.currentDescriptionIndex = (this.currentDescriptionIndex + 1) % this.descriptions.length;
+
+      await this.delay(5000);
+
+      while (this.currentDescription.length > 0) {
+        this.currentDescription = this.currentDescription.slice(0, -1);
+        await this.delay(100);
+      }
+
+      await this.delay(1000);
+
+      const targetDescription = this.descriptions[this.currentDescriptionIndex];
+      for (let i = 0; i <= targetDescription.length; i++) {
+        this.currentDescription = targetDescription.slice(0, i);
+        await this.delay(250);
+      }
+    }
+  }
   }
   
 }
